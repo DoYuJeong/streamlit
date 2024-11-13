4481bf89f1c0361360a97f61e863d5a8523233de
DoYuJeong <148860776+DoYuJeong@users.noreply.github.com>
2024-11-13 10:03:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ import streamlit as st
 
 st.title("도유정")
 # st.write("도유정, 박주은, 윤부성, 김종진")
+
+link = st.link_button("Go to Data download", url="https://figshare.com/articles/dataset/20241101_starrydata2/27390096?file=50120700")
+
+
